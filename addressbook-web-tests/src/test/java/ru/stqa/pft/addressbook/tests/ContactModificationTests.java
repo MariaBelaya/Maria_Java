6b0f2d64094f41aa621c4449b8057189b6ba6464
Maria <maria.franova@yandex.ru>
2018-10-04 19:39:15 +0300

    изменения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -21,7 +21,6 @@ public class ContactModificationTests extends TestBase {
     }
     List<ContactData> before = app.getContactHelper().getContactList();
 
-    app.getContactHelper().selectContact(before.size() - 1);
     ContactData contact = new ContactData(before.get(before.size() - 1).getId(),"Maria", "Belaya", "Moscow", "89123456789", "test@test.ru", null);
     app.getContactHelper().openEditContactPage();
     app.getContactHelper().fillContactForm(contact);
