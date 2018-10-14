85710b915552fbfeeee39b36fd64c38e8c83ee3d
Maria <maria.franova@yandex.ru>
2018-10-14 18:52:49 +0300

    Лекция 6.4. Параметризация тестовых методов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,41 +1,53 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() {
-    app.goTo().homePage();
-    Contacts before = app.contact().all();
-    app.contact().goToAddNewContactPage();
-    File photo = new File("src/test/resources/linux.png");
-    ContactData contact = new ContactData().withSurname("Belaya").withName("Maria").withPhoto(photo).withAddress("Moscow")
-            .withHomePhone("5467890").withMobilPhone("8948372839").withWorkPhone("687932424").withEmail1("test@test.ru")
-            .withEmail2("test1@test.ru").withEmail3("test2@test.ru");
-    app.contact().create(contact, true);
-
-    try {
-      Thread.sleep(3000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-
-    System.out.println(app.contact().count());
-    System.out.println(before.size() + 1);
-    assertThat(app.contact().count(), equalTo(before.size() + 1));
-    Contacts after = app.contact().all();
-    assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new ContactData().withName("Maria 1").withSurname("Belaya 1")
+            .withAddress("Moscow 1").withEmail1("test1@test.ru").withHomePhone("1111111")});
+    list.add(new Object[]{new ContactData().withName("Maria 2").withSurname("Belaya 2")
+            .withAddress("Moscow 2").withEmail1("test2@test.ru").withHomePhone("2222222")});
+    list.add(new Object[]{new ContactData().withName("Maria 3").withSurname("Belaya 3")
+            .withAddress("Moscow 3").withEmail1("test3@test.ru").withHomePhone("3333333")});
+    return list.iterator();
   }
 
+  @Test (dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
+      File photo = new File("src/test/resources/linux.png");
+      app.goTo().homePage();
+      Contacts before = app.contact().all();
+      app.contact().goToAddNewContactPage();
+      app.contact().create(contact, true);
+
+      try {
+        Thread.sleep(3000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+
+      System.out.println(app.contact().count());
+      System.out.println(before.size() + 1);
+      assertThat(app.contact().count(), equalTo(before.size() + 1));
+      Contacts after = app.contact().all();
+      assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    }
 
 
   @Test (enabled = false)
