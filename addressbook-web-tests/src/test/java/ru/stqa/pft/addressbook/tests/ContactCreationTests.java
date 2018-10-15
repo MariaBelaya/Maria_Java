fb252a32a2ca2d342dd747d6cd22a182694b86de
Maria <maria.franova@yandex.ru>
2018-10-15 14:08:08 +0300

    правки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -19,39 +19,39 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-//  @DataProvider
-//  public Iterator<Object[]> validContacts() throws IOException {
-//    List<Object[]> list = new ArrayList<Object[]>();
-//    BufferedReader reader = new BufferedReader(new FileReader((new File("src/test/resources/contacts.csv"))));
-//    String line = reader.readLine();
-//    while (line != null) {
-//      String[] split = line.split(";");
-//      list.add(new Object[]{new ContactData().withName(split[0]).withSurname(split[1])
-//              .withAddress(split[2]).withHomePhone(split[3]).withEmail1(split[4])});
-//      line = reader.readLine();
-//    }
-//    return list.iterator();
-//  }
-//
-//  @Test (dataProvider = "validContacts")
-//  public void testContactCreation(ContactData contact) throws Exception {
-//    File photo = new File("src/test/resources/linux.png");
-//    Contacts before = app.contact().all();
-//    app.contact().goToAddNewContactPage();
-//    app.contact().create(contact, true);
-//
-//    try {
-//      Thread.sleep(3000);
-//    } catch (InterruptedException e) {
-//      e.printStackTrace();
-//    }
-//
-//    System.out.println(app.contact().count());
-//    System.out.println(before.size() + 1);
-//    assertThat(app.contact().count(), equalTo(before.size() + 1));
-//    Contacts after = app.contact().all();
-//    assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
-//  }
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader((new File("src/test/resources/contacts.csv"))));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new ContactData().withName(split[0]).withSurname(split[1])
+              .withAddress(split[2]).withHomePhone(split[3]).withEmail1(split[4])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @Test (dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
+    File photo = new File("src/test/resources/linux.png");
+    Contacts before = app.contact().all();
+    app.contact().goToAddNewContactPage();
+    app.contact().create(contact, true);
+
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    System.out.println(app.contact().count());
+    System.out.println(before.size() + 1);
+    assertThat(app.contact().count(), equalTo(before.size() + 1));
+    Contacts after = app.contact().all();
+    assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+  }
 
 
   @Test 
