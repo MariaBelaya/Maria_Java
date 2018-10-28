8a9ba6f41e4c6afccb2dec22e51bc1fb6c727ae4
Maria <maria.franova@yandex.ru>
2018-10-28 14:49:12 +0300

    мантис

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,48 +1,126 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
-import org.openqa.selenium.safari.SafariDriver;
+
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.MatchResult;
 
 public class ApplicationManager {
   private final Properties properties;
-  WebDriver wd;
-
+  private WebDriver wd;
   private String browser;
+  private RegistrationHelper registrationHelper;
+  private FtpHelper ftp;
+  private MailHelper mailHelper;
+  private JamesHelper jamesHelper;
+  private SessionHelper sessionHelper;
+  private NavigationHelper navigationHelper;
+  private UserManagmentHelper userManagmentHelper;
+  private DbHelper dbHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
     properties = new Properties();
   }
 
-  public void init() throws IOException {
+  public void init() throws Exception {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.SAFARI)) {
-      wd = new SafariDriver();
-    }
-
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
+    dbHelper = new DbHelper();
+  }
 
 
-    public void stop() { wd.quit();
+  public void stop() {
+    if (wd != null) {
+      wd.quit();
     }
   }
+
+  public HttpSession newSession() {
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
+  }
+
+  public FtpHelper ftp(){
+    if(ftp == null){
+      ftp = new FtpHelper(this);
+    }
+    return ftp;
+  }
+
+  public MailHelper mail(){
+    if(mailHelper == null){
+      mailHelper = new MailHelper(this);
+    }
+    return mailHelper;
+  }
+
+  public JamesHelper james (){
+    if (jamesHelper == null){
+      jamesHelper = new JamesHelper(this);
+    }
+    return jamesHelper;
+  }
+
+  public SessionHelper session(){
+    if(sessionHelper == null){
+      sessionHelper = new SessionHelper(this);
+    }
+    return sessionHelper;
+  }
+
+  public NavigationHelper goTo(){
+    if(navigationHelper == null){
+      navigationHelper = new NavigationHelper(this);
+    }
+    return navigationHelper;
+  }
+
+  public UserManagmentHelper user(){
+    if(userManagmentHelper == null){
+      userManagmentHelper = new UserManagmentHelper(this);
+    }
+    return userManagmentHelper;
+  }
+
+  public DbHelper db(){
+    return dbHelper;
+  }
+
 }
-
-
-
---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/model/UserData.java
@@ -0,0 +1,57 @@
+package ru.stqa.pft.mantis.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "mantis_user_table")
+public class UserData {
+
+  @Id
+  @Column(name = "id")
+  private int id;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "email")
+  private String email;
+
+  @Transient
+  private String password;
+
+  public int getId() {
+    return id;
+  }
+
+  public UserData withId(int id) {
+    this.id = id;
+    return this;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public UserData withUsername(String username) {
+    this.username = username;
+    return this;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public UserData withEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
+  public String getPassword(){
+    return password;
+  }
+
+  public UserData withPassword(String password){
+    this.password = password;
+    return this;
+  }
+}
