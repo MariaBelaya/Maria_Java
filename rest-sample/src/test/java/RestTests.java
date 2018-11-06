0009a77b7f6a2dcf0aa8c8a71a591dd38455ea86
Maria <maria.franova@yandex.ru>
2018-11-06 12:23:49 +0300

    правки

---
+++ rest-sample/src/test/java/RestTests.java
@@ -0,0 +1,48 @@
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.Executor;
+
+import static org.testng.Assert.assertEquals;
+
+public class RestTests {
+
+  @Test
+  public void testCreateIssue() throws IOException {
+    Set<Issue> oldIssues = getIssues();
+    Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
+    int issueId = createIssue(newIssue);
+    Set<Issue> newIssues = getIssues();
+    oldIssues.add(newIssue.withId(issueId));
+    assertEquals(newIssues, oldIssues);
+  }
+
+
+  private Set<Issue> getIssues() throws IOException {
+    String json = getExecutor().execute(Get("http://bugify.stqa.ru/api/issues.json"))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement issues = parsed.getAsJsonObject().get("issues");
+
+    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
+  }
+
+  private Executor getExecutor() {
+    return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+  }
+
+  private int createIssue(Issue newIssue) throws IOException {
+    String json = getExecutor()
+            .execute(Post("http://bugify.stqa.ru/api/issues.json")
+                    .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
+                            new BasicNameValuePair("description", newIssue.getDescription())))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    return parsed.getAsJsonObject().get("issue_id").getAsInt();
+  }
+
+
+
+}
