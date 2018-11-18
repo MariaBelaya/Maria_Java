a4d1936fb7d0ded945aac54e6517385c96834633
Maria <maria.franova@yandex.ru>
2018-11-18 16:04:39 +0300

    12345

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -26,61 +26,61 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
+//
+//  @DataProvider
+//  public Iterator<Object[]> validContactsFromXml() throws IOException {
+//    List<Object[]> list = new ArrayList<Object[]>();
+//    try (BufferedReader reader = new BufferedReader(new FileReader((new File("src/test/resources/contacts.xml"))))) {
+//      String xml = "";
+//      String line = reader.readLine();
+//      while (line != null) {
+//        xml += line;
+//        line = reader.readLine();
+//      }
+//      XStream xstream = new XStream();
+//      xstream.processAnnotations(ContactData.class);
+//      List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+//      return contacts.stream().map((contactData) -> new Object[]{contactData}).collect(Collectors.toList()).iterator();
+//    }
+//  }
+//
+//  @DataProvider
+//  public Iterator<Object[]> validContactsFromJson() throws IOException {
+//    List<Object[]> list = new ArrayList<Object[]>();
+//    try (BufferedReader reader = new BufferedReader(new FileReader((new File("src/test/resources/contacts.json"))))) {
+//      String json = "";
+//      String line = reader.readLine();
+//      while (line != null) {
+//        json += line;
+//        line = reader.readLine();
+//      }
+//
+//      Gson gson = new Gson();
+//      List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+//      }.getType());
+//      return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
+//    }
+//  }
 
-  @DataProvider
-  public Iterator<Object[]> validContactsFromXml() throws IOException {
-    List<Object[]> list = new ArrayList<Object[]>();
-    try (BufferedReader reader = new BufferedReader(new FileReader((new File("src/test/resources/contacts.xml"))))) {
-      String xml = "";
-      String line = reader.readLine();
-      while (line != null) {
-        xml += line;
-        line = reader.readLine();
-      }
-      XStream xstream = new XStream();
-      xstream.processAnnotations(ContactData.class);
-      List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
-      return contacts.stream().map((contactData) -> new Object[]{contactData}).collect(Collectors.toList()).iterator();
-    }
-  }
-
-  @DataProvider
-  public Iterator<Object[]> validContactsFromJson() throws IOException {
-    List<Object[]> list = new ArrayList<Object[]>();
-    try (BufferedReader reader = new BufferedReader(new FileReader((new File("src/test/resources/contacts.json"))))) {
-      String json = "";
-      String line = reader.readLine();
-      while (line != null) {
-        json += line;
-        line = reader.readLine();
-      }
-
-      Gson gson = new Gson();
-      List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
-      }.getType());
-      return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
-    }
-  }
-
-  @Test(dataProvider = "validContactsFromJson")
-  public void testContactCreationJson(ContactData contact) throws Exception {
-    Contacts before = app.db().contacts();
-    app.contact().goToAddNewContactPage();
-    app.contact().create(contact, true);
-
-    try {
-      Thread.sleep(2000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-
-    System.out.println(app.contact().count());
-    System.out.println(before.size() + 1);
-    Contacts after = app.db().contacts();
-    assertThat(app.contact().count(), equalTo(before.size() + 1));
-    assertThat(after, equalTo
-            (before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
-  }
+//  @Test(dataProvider = "validContactsFromJson")
+//  public void testContactCreationJson(ContactData contact) throws Exception {
+//    Contacts before = app.db().contacts();
+//    app.contact().goToAddNewContactPage();
+//    app.contact().create(contact, true);
+//
+//    try {
+//      Thread.sleep(2000);
+//    } catch (InterruptedException e) {
+//      e.printStackTrace();
+//    }
+//
+//    System.out.println(app.contact().count());
+//    System.out.println(before.size() + 1);
+//    Contacts after = app.db().contacts();
+//    assertThat(app.contact().count(), equalTo(before.size() + 1));
+//    assertThat(after, equalTo
+//            (before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+//  }
 
   @Test
   public void testContactCreation() {
