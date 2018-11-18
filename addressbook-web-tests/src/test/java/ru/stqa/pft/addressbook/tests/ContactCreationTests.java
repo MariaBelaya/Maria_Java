4150e9982e5a5cf8852ec0b56699d905cde261a4
Maria <maria.franova@yandex.ru>
2018-11-18 20:41:42 +0300

    12345

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -82,6 +82,7 @@ public class ContactCreationTests extends TestBase {
 //            (before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
 //  }
 
+
   @Test
   public void testContactCreation() {
     Groups groups = app.db().groups();
